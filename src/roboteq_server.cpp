80e637f56bc5b56cc7924e55ad4b48a6828bb637
harsh-99 <harshmaheshwari135@gmail.com>
2018-03-20 19:31:11 +0530

    updated with feedback

--- src/roboteq_server.cpp
+++ src/roboteq_server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include "geometry_msgs/Twist.h"
 
 #include "ros/ros.h"
 #include "eklavya4_roboteq/SetSpeed.h"
@@ -10,12 +11,13 @@
 #include "eklavya4_roboteq/Constants.h"
 
 using namespace std;
-
+#define r 10   //the radius of wheel in centimetre
+geometry_msgs::Twist vel_msg;
+ros::Publisher controller_pub;
 RoboteqDevice device;
 int status = 0;
 
 bool setSpeed(eklavya4_roboteq::SetSpeed::Request &req, eklavya4_roboteq::SetSpeed::Response &res) {
-    
     res.code = 0;
     
 	if((status = device.SetCommand(_GO, 1, req.v_r)) != RQ_SUCCESS) {
@@ -31,7 +33,29 @@ bool setSpeed(eklavya4_roboteq::SetSpeed::Request &req, eklavya4_roboteq::SetSpe
 	else {
 		ROS_DEBUG("Succeeded.");
 	}
-
+	/************************FEEDBACK*****************************/
+	int left_speed=0,right_speed=0;
+	if((status = device.GetValue(_ABSPEED, 1, left_speed)) != RQ_SUCCESS) {
+		ROS_INFO("Failed... Error code --> ", status);
+	}
+	else {
+		ROS_DEBUG("Succeeded.");
+	}
+		
+	usleep(100);
+	
+	if (status == 0) {
+		
+		if((status = device.GetValue(_ABSPEED, 2, right_speed)) != RQ_SUCCESS) {
+			ROS_INFO("Failed... Error code --> ", status);
+		}
+		else {
+			ROS_DEBUG("Succeeded.");
+		}
+	}
+	vel_msg.linear.x=(left_speed*2*3.1415*r)/60;             //To convert velocity in cm/s from RPM
+	vel_msg.linear.y=(right_speed*2*3.1415*r)/60;
+	/****************************FEEDBACK**************************/
 	usleep(100);
 	
 	int numberOfattempts = 0;
@@ -58,37 +82,6 @@ bool setSpeed(eklavya4_roboteq::SetSpeed::Request &req, eklavya4_roboteq::SetSpe
     
     return true;
 }
-
-/*bool getSpeed(eklavya_roboteq::GetSpeed::Request &req, eklavya_roboteq::GetSpeed::Response &res) {
-	ROS_INFO("request: Encoder speed data.");
-	
-	int left_speed = 0, right_speed = 0;
-    
-	if((status = device.GetValue(_ABSPEED, 1, left_speed)) != RQ_SUCCESS) {
-		ROS_INFO("Failed... Error code --> ", status);
-	}
-	else {
-		ROS_DEBUG("Succeeded.");
-	}
-		
-	usleep(100);
-	
-	if (status == 0) {
-		
-		if((status = device.GetValue(_ABSPEED, 2, right_speed)) != RQ_SUCCESS) {
-			ROS_INFO("Failed... Error code --> ", status);
-		}
-		else {
-			ROS_DEBUG("Succeeded.");
-			return true;
-		}
-	}
-		
-	res.left_speed = left_speed;
-	res.right_speed = right_speed;
-	
-	return true;
-}*/
 
 int main(int argc, char **argv)
 {
@@ -117,11 +110,30 @@ int main(int argc, char **argv)
 	}
   
     ros::ServiceServer service1 = n.advertiseService("motor_controller", setSpeed);
+    controller_pub = n.advertise<geometry_msgs::Twist>("result_vel",1000);
     //ros::ServiceServer service2 = n.advertiseService("motor_speed", getSpeed);
     ROS_INFO("Server initialized...");
     ROS_INFO("Ready to control motors...");
-    
-    ros::spin();
+    ros::Rate loop_rate(10.0);
+	
+	vel_msg.linear.x=0;
+	vel_msg.angular.z=0;
+	vel_msg.linear.y=0;
+	vel_msg.linear.z=0;
+	vel_msg.angular.x=0;
+	vel_msg.angular.y=0;
+      
+	controller_pub.publish(vel_msg);
+	
+	loop_rate.sleep();
+	
+	while(ros::ok())
+	{
+	controller_pub.publish(vel_msg);
+	ros::spinOnce();
+	ROS_INFO("%f", vel_msg.linear.x);
+	}    
+    //ros::spin();
 	
 	device.Disconnect();
   
